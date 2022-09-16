6c257b97d47ead7b10ac582edab7ac676f200329
Tatiana Viana <tatisabinoviana@gmail.com>
2022-09-15 21:24:08 -0300

    Iniciando o backend

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express, { request, response } from 'express';
+
+const app = express();
+
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+app.post('/ads', (request, response) => {
+  return response.json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
+
+  return response.json([
+    { id: 1, name: 'Anúncio 1' },
+    { id: 2, name: 'Anúncio 2' },
+    { id: 3, name: 'Anúncio 3' },
+    { id: 4, name: 'Anúncio 4' },
+    { id: 5, name: 'Anúncio 5' }
+  ]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  // const adId = request.params.id;
+
+  return response.json([]);
+});
+
+app.listen(3333);
